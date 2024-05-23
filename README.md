9e8bb22a490523c1fb84c88e856ade1dfe8ec414
Fernando Perry <fernandoperry1234@gmail.com>
2024-05-23 23:55:09 +0700

    added readme, etc

---
+++ README.md
@@ -0,0 +1,27 @@
+# MPRIS-STATUS
+a simple program that will output the current media metadata or status.
+
+## BUILD
+
+```sh
+cargo build --release
+```
+
+## USAGE
+
+to display status :
+
+```sh
+./build/release/mpris-status s
+# or
+./build/release/mpris-status status
+```
+
+
+to diplay metadata :
+
+```sh
+./build/release/mpris-status m
+# or
+./build/release/mpris-status metadata
+```
---
+++ src/main.rs
@@ -0,0 +1,140 @@
+use mpris::{PlaybackStatus, PlayerFinder};
+
+enum ProgramMode {
+    STATUS,
+    METADATA,
+}
+
+fn connect() -> Result<mpris::Player, ()> {
+    let player = match PlayerFinder::new() {
+        Err(_) => return Err(()),
+        Ok(v) => v,
+    }
+    .find_active();
+    if let Ok(player) = player {
+        return Ok(player);
+    } else {
+        return Err(());
+    }
+}
+
+fn get_metadata(player_name: &mpris::Player) -> Vec<String> {
+    let mut data: Vec<String> = Vec::new();
+    let metadata = match player_name.get_metadata() {
+        Err(_) => {
+            data.push("Not available".to_string()); // title will be `Not available`
+            data.push("None".to_string()); // artist will be `None`
+            return data;
+        }
+        Ok(v) => v,
+    };
+    if let Some(mpris::MetadataValue::String(title)) = metadata.get("xesam:title") {
+        data.push(title.to_owned());
+    } else {
+        data.push("Not available".to_string());
+    };
+    if let Some(mpris::MetadataValue::Array(artist)) = metadata.get("xesam:artist") {
+        if artist.len() > 1 {
+            let mut data_to_push: String = String::new();
+            for n in 0..artist.len() {
+                if let Some(mpris::MetadataValue::String(artist_str)) = artist.get(n) {
+                    match n {
+                        0 => data_to_push.push_str(&artist_str.to_string()),
+                        _ => data_to_push.push_str(&format!(", {artist_str}")),
+                    };
+                } else {
+                    data.push("None".to_string());
+                }
+            }
+        } else {
+            if let Some(mpris::MetadataValue::String(artist_str)) = artist.get(0) {
+                data.push(artist_str.to_string());
+            } else {
+                data.push("None".to_string());
+            }
+        }
+    };
+    if data.len() < 2 {
+        data.push("None".to_string());
+    } else if data[1].chars().count() < 1 {
+        data[1].push_str(&"None");
+    }
+    return data;
+}
+
+fn get_status(player: &mpris::Player) -> Result<(String, PlaybackStatus), ()> {
+    let player_name : String = player.bus_name_player_name_part().to_string();
+    let player_status= mpris::Player::get_playback_status(&player);
+    if let Ok(value) = player_status {
+        return Ok((player_name, value));
+    } else {
+        return Err(());
+    };
+}
+
+fn get_args() -> Option<ProgramMode> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 1{
+        return None;
+    }
+    let mut return_value: ProgramMode = ProgramMode::METADATA;
+    let mut counter: usize = 1;
+    while counter < args.len(){
+        match &args[counter][..]{
+            "status" | "s" => {
+                counter += 1;
+                return_value = ProgramMode::STATUS;
+            }
+            "metadata" | "m" => {
+                counter += 1;
+                return_value = ProgramMode::METADATA;
+            }
+            _ => {
+                return None;
+            }
+        }
+        counter += 1;
+    }
+    return Some(return_value);
+}
+
+fn main() {
+    let args: Option<ProgramMode> = get_args();
+    let player = connect();
+    match player {
+        Err(_) => {
+            println!("No player available.");
+            // eprintln!("ERR : Cant get the player!");
+            std::process::exit(1);
+        }
+        Ok(v) => {
+            match args {
+                Some(ProgramMode::STATUS) => {
+                    match get_status(&v) {
+                        Ok((s, PlaybackStatus::Paused)) => {
+                            println!("{} currently Paused", s.to_uppercase());
+                        }
+                        Ok((s,PlaybackStatus::Playing)) => {
+                            println!("{} currently Playing", s.to_uppercase());
+                        }
+                        Ok((s,PlaybackStatus::Stopped)) => {
+                            println!("{} currently Stopped", s.to_uppercase());
+                        }
+                        Err(()) => {
+                            eprintln!("ERR : Cant get the player current status");
+                            std::process::exit(2);
+                        }
+                    };
+                }
+                Some(ProgramMode::METADATA) => {
+                    let metadata: Vec<String> = get_metadata(&v);
+                    println!("{} - {}", metadata[0], metadata[1]);
+                }
+                None => {
+                    eprintln!("ERR : Did't get valid args.");
+                    std::process::exit(3);
+                }
+            }
+        }
+    }
+}
